4d7561057eaa6415a6ad4a7212a2ce648de81459
Becky Lancaster <lcbeck@yahoo.com>
2019-11-18 10:09:10 -0600

    updated

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavPhotosComponent implements OnInit {
   photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image1 = 'https://m.media-amazon.com/images/M/MV5BMjIwMjE1Nzc4NV5BMl5BanBnXkFtZTgwNDg4OTA1NzM@._V1_UX182_CR0,0,182,268_AL_.jpg';
+  image2 = 'https://m.media-amazon.com/images/M/MV5BMjAyNDEyMzc4Ml5BMl5BanBnXkFtZTgwMjEzNjM0NTM@._V1_UX182_CR0,0,182,268_AL_.jpg';
+  image3 = 'https://m.media-amazon.com/images/M/MV5BM2MwMTIzYmUtZmE2Yy00MDNjLTllYmEtNmE4YTIyZTljOWI3XkEyXkFqcGdeQXVyMjM0MTIzNDg@._V1_UX182_CR0,0,182,268_AL_.jpg';
 
   constructor() { }
 
